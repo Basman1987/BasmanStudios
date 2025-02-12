17d88edd348f9049df734a0b438bfb4a532a153b
Basman1987 <basmandis@gmail.com>
2025-02-12 11:22:11 +0100

    Add files via upload
    New footer File

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 flex items-center justify-center">
         <div className="flex items-center space-x-2">
           <div className="w-8 h-8 rounded-full overflow-hidden bg-gray-800">
-            <Image src="/Basman_Logo_500.webp?height=32&width=32" alt="Basman logo" width={32} height={32} />
+            <Image src="/Basman_Logo_500.webp" alt="Basman logo" width={32} height={32} loading="lazy" />
           </div>
           <p className="text-sm text-gray-400">© 2025 Basman Studios | Made by Basman</p>
         </div>
